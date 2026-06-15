cd0eb6e3879e9fa75798b091e4aac4c0ec392c1c
Valdarey <prabaswaradaffa2@gmail.com>
2026-06-16 02:07:59 +0700

    fix 1

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 # ── Konfigurasi halaman ───────────────────────────────────────────────────────
 st.set_page_config(
